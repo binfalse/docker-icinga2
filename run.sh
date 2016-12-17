d9efc4c6a9170b3d44f8a2aeb96639c6aa4d6e0b
Martin Scharm <martin@binfalse.de>
2016-12-17 21:35:42 +0100

    now waiting for the db to come up
    previously, the run script didn't fill the database, because
    the db was too slow to some up...
    now the script waits for the db-link. credits go to:
    https://github.com/dominionenterprises/tol-api-php/blob/master/tests/provisioning/set-env.sh

--- run.sh
+++ run.sh
@@ -5,16 +5,35 @@ mkdir -p /run/icinga2/
 chmod 777 /run/icinga2/
 
 
+# check if database is up and running
+# ref: https://github.com/dominionenterprises/tol-api-php/blob/master/tests/provisioning/set-env.sh
+if [ -z "${DBHOST}" ]
+then
+    echo "you need to link this container with a mysql db"
+    exit 1
+fi
+[ -z "${DBPORT}" ] && export DBPORT=3306
+
+while ! exec 6<>/dev/tcp/"${DBHOST}"/"${DBPORT}" > /dev/null 2>&1; do
+    echo "$(date) - still waiting for mysql at ${DBHOST}:${DBPORT} to come up"
+    sleep 1
+done
+
+exec 6>&-
+exec 6<&-
+
+
+
 # is the database already good to go or do we need to set it up?
 # if there is no table `icinga` we need to create it
-rows=$(mysqlshow -u "$DBUSER" "-p$DBPASS" -h "$DBHOST" "$DBNAME" 2>/dev/null | wc -l)
+rows=$(mysqlshow -u "$DBUSER" "-p$DBPASS" -h "$DBHOST" -P "${DBPORT}" "$DBNAME" 2>/dev/null | wc -l)
 
 if [ "$?" != 0 ] && [ "$rows" -gt 5 ]
 then
     # ok, this is the first run, so let's setup the database
-    echo "CREATE DATABASE $DBNAME;" | mysql -u "$DBUSER" "-p$DBPASS" -h "$DBHOST"
-    echo "SET @@global.sql_mode='MYSQL40';" | mysql -u "$DBUSER" "-p$DBPASS" -h "$DBHOST"
-    mysql -u "$DBUSER" "-p$DBPASS" -h "$DBHOST" "$DBNAME" < /usr/share/icinga2-ido-mysql/schema/mysql.sql
+    echo "CREATE DATABASE $DBNAME;" | mysql -u "$DBUSER" "-p$DBPASS" -h "$DBHOST" -P "${DBPORT}"
+    echo "SET @@global.sql_mode='MYSQL40';" | mysql -u "$DBUSER" "-p$DBPASS" -h "$DBHOST" -P "${DBPORT}"
+    mysql -u "$DBUSER" "-p$DBPASS" -h "$DBHOST" -P "${DBPORT}" "$DBNAME" < /usr/share/icinga2-ido-mysql/schema/mysql.sql
 fi
 
 
