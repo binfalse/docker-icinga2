e930b53de8ecefe28db32db5ceff392b72cc2aca
Martin Scharm <martin@binfalse.de>
2016-12-17 20:24:03 +0100

    initial version of the docker image for icinga2+ido

---
+++ run.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# setup env for icinga
+mkdir -p /run/icinga2/
+chmod 777 /run/icinga2/
+
+
+# is the database already good to go or do we need to set it up?
+# if there is no table `icinga` we need to create it
+mysqlshow -u "$DBUSER" "-p$DBPASS" -h "$DBHOST" "$DBNAME" >/dev/null 2>&1
+
+if [ "$?" != 0 ]
+then
+    # ok, this is the first run, so let's setup the database
+    echo "CREATE DATABASE $DBNAME;" | mysql -u "$DBUSER" "-p$DBPASS" -h "$DBHOST"
+    echo "SET @@global.sql_mode='MYSQL40';" | mysql -u "$DBUSER" "-p$DBPASS" -h "$DBHOST"
+    mysql -u "$DBUSER" "-p$DBPASS" -h "$DBHOST" "$DBNAME" < /usr/share/icinga2-ido-mysql/schema/mysql.sql
+fi
+
+
+# and we're going for an infinte while loop
+# while dumping icinga's log to std::out
+#
+# this way we can restart icinga by just shutting it down
+# (when restarting icinga it will go deamon
+# and thus it won't print to std::out anymore...)
+while true
+do
+    icinga2 daemon
+
+    # sleeping for a bit gives us a chance to quit
+    # the run.sh with a double ctrl+c
+    sleep 2
+done
+
+
