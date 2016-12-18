9c99e521453b4ef294d7dcd7533983ddbdf9dd0a
Martin Scharm <martin@binfalse.de>
2016-12-18 12:54:14 +0100

    improved the wrapper script

--- run.sh
+++ run.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
+# the following env variables should be set:
+#
+# DBHOST -- the host name of the MySQL database
+# DBPORT -- the port of the MySQL database,
+#           defaults to 3306 if unset
+# DBROOT -- the root passwort of the databse,
+#           is required if we need to setup the databse
+# DBNAME -- the name of the database
+# DBUSER -- the username of the database user
+# DBPASS -- the password of the database user
+#
+
 
 # setup env for icinga
 mkdir -p /run/icinga2/
 chmod 777 /run/icinga2/
 
 
-# check if database is up and running
-# ref: https://github.com/dominionenterprises/tol-api-php/blob/master/tests/provisioning/set-env.sh
+# check if database is up and running, credits go to:
+# https://github.com/dominionenterprises/tol-api-php/blob/master/tests/provisioning/set-env.sh
 if [ -z "${DBHOST}" ]
 then
     echo "you need to link this container with a mysql db"
@@ -14,7 +26,8 @@ then
 fi
 [ -z "${DBPORT}" ] && export DBPORT=3306
 
-while ! exec 6<>/dev/tcp/"${DBHOST}"/"${DBPORT}" > /dev/null 2>&1; do
+while ! exec 6<>/dev/tcp/"${DBHOST}"/"${DBPORT}"
+do
     echo "$(date) - still waiting for mysql at ${DBHOST}:${DBPORT} to come up"
     sleep 1
 done
@@ -22,18 +35,21 @@ done
 exec 6>&-
 exec 6<&-
 
+# ok, databse is up.
 
 
 # is the database already good to go or do we need to set it up?
 # if there is no table `icinga` we need to create it
 rows=$(mysqlshow -u "$DBUSER" "-p$DBPASS" -h "$DBHOST" -P "${DBPORT}" "$DBNAME" 2>/dev/null | wc -l)
 
-if [ "$?" != 0 ] && [ "$rows" -gt 5 ]
+if [ "$?" != 0 ] || [ "$rows" -lt 10 ]
 then
     # ok, this is the first run, so let's setup the database
-    echo "CREATE DATABASE $DBNAME;" | mysql -u "$DBUSER" "-p$DBPASS" -h "$DBHOST" -P "${DBPORT}"
-    echo "SET @@global.sql_mode='MYSQL40';" | mysql -u "$DBUSER" "-p$DBPASS" -h "$DBHOST" -P "${DBPORT}"
+    echo "installing database for icinga2-ido-mysql"
+    echo "CREATE DATABASE IF NOT EXISTS $DBNAME;" | mysql -u "$DBUSER" "-p$DBPASS" -h "$DBHOST" -P "${DBPORT}"
+    echo "SET @@global.sql_mode='MYSQL40';" | mysql -u root "-p$DBROOT" -h "$DBHOST" -P "${DBPORT}"
     mysql -u "$DBUSER" "-p$DBPASS" -h "$DBHOST" -P "${DBPORT}" "$DBNAME" < /usr/share/icinga2-ido-mysql/schema/mysql.sql
+    echo "mysql scheme for icinga2-ido-mysql imported"
 fi
 
 
@@ -41,10 +57,11 @@ fi
 # while dumping icinga's log to std::out
 #
 # this way we can restart icinga by just shutting it down
-# (when restarting icinga it will go deamon
+# (when restarting icinga it will go background daemon
 # and thus it won't print to std::out anymore...)
 while true
 do
+    echo "starting icinga2 daemon"
     icinga2 daemon
 
     # sleeping for a bit gives us a chance to quit
